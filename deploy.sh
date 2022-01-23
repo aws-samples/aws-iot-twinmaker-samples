28ddef7a3a79b21330ac30ba5911da4df96fe885
Ubuntu <ubuntu@ip-172-31-75-4.ec2.internal>
2022-01-23 04:52:41 +0000

    update deploy notes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+# Some notes:
+# Easier in Ubuntu than in Amazon Linux
+# If using Cloud 9 will need to increase disk size
+
+
+
 # Set arguments as environment variables
 export CDK_DEFAULT_ACCOUNT=$(aws sts get-caller-identity --query "Account" --output text) 
 export GETTING_STARTED_DIR=$PWD
@@ -63,25 +69,31 @@ sudo apt install libgl1-mesa-glx # to address issue with OpenCV
 # Also, have added a few extra steps in the pip install, see above
 
 # Deploy an Instance of the Timestream Telemetry module.
+# Uncomment the first time running for each AWS tenant. Creates the resources
 # cd $GETTING_STARTED_DIR/src/modules/timestream_telemetry/cdk/
 # npm install
 # cdk deploy
 
 
-# STUCK ABOVE; MAY BE BECUASE I RAN OUT OF DISK SPACE
-# TRY MOVING TO ANOTHER UBUNTU MACHINE WITH MORE DISK SPACE
-# Instrucions here: https://docs.aws.amazon.com/cloud9/latest/user-guide/move-environment.html
-
-
-
-
-
-
-# cd $GETTING_STARTED_DIR/src/workspaces/cookiefactory/
 
 # import cookie factory data into your workspace
+# Only do one time for each timestream telemetry stack
+# Uncomment the first time (for each local environment or each AWS environment?)
+# cd $GETTING_STARTED_DIR/src/workspaces/cookiefactory/
 # python3 -m setup_content \
 #   --telemetry-stack-name $TIMESTREAM_TELEMETRY_STACK_NAME \
 #   --workspace-id $WORKSPACE_ID \
 #   --region-name $AWS_DEFAULT_REGION \
 #   --import-all
+
+# Test that data has been added by running the following query. If not available, uncomment above to import content into cookiefactory TwinMaker instance
+aws iottwinmaker get-property-value-history \
+   --region $AWS_DEFAULT_REGION \
+   --cli-input-json '{"componentName": "AlarmComponent","endDateTime": "2022-11-01T00:00:00","entityId": "Mixer_2_06ac63c4-d68d-4723-891a-8e758f8456ef","orderByTime": "ASCENDING","selectedProperties": ["alarm_status"],"startDateTime": "2021-11-01T00:00:00","workspaceId": "'${WORKSPACE_ID}'"}'
+   
+# Now deploy Grafana on your instance or on the web
+# Uncomment if you have not previously deployed Grafana on the local machine
+sh $GETTING_STARTED_DIR/src/modules/grafana/setup_cloud9_grafana_docker.sh
+
+# Error
+# Error starting userland proxy: listen tcp4 0.0.0.0:80: bind: address already in use.
