3080aa4f2e52813fff5a8cdb5af0caea96df5602
Ubuntu <ubuntu@ip-172-31-75-4.ec2.internal>
2022-01-23 04:15:13 +0000

    update notes

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ echo "WORKSPACE_ID: ${WORKSPACE_ID}"
 
 # run from same directory as this README
 # Uncomment and use if your AWS CLI version is less than 1.22.17
-#aws configure add-model --service-model file://iottwinmaker-2021-11-29.normal.json
+# aws configure add-model --service-model file://iottwinmaker-2021-11-29.normal.json
 
 # You will also need to bootstrap your account for CDK so that custom assets, such as sample Lambda functions, can be easily deployed. Use the following command.
 cdk bootstrap aws://$CDK_DEFAULT_ACCOUNT/$CDK_DEFAULT_REGION
@@ -34,7 +34,11 @@ aws ecr-public get-login-password --region $CDK_DEFAULT_REGION | docker login --
 source env/bin/activate
 
 # Install Python Libraries
-pip3 install -r $GETTING_STARTED_DIR/src/workspaces/cookiefactory/requirements.txt
+# pip3 install scikit-build # to resolve an error during pip install
+
+pip install --upgrade pip
+pip install -r $GETTING_STARTED_DIR/src/workspaces/cookiefactory/requirements.txt
+sudo apt install libgl1-mesa-glx # to address issue with OpenCV
 
 
 # Create IoT TwinMaker Workspace
@@ -44,21 +48,40 @@ pip3 install -r $GETTING_STARTED_DIR/src/workspaces/cookiefactory/requirements.t
 # In the console, create an IoT TwinMaker Workspace using the Role ARN that was output when you ran the above command
 # Use instructions in the README of this repo
 
-# Run the following script to create a role for accessing the workspace in a Grafana dashboard
-# Note the ARN of the role you create. You will use it when configuring a data source in Grafana.
-python3 $GETTING_STARTED_DIR/src/modules/grafana/create_grafana_dashboard_role.py --workspace-id $WORKSPACE_ID --region $AWS_DEFAULT_REGION
-
-# Stuck above. Getting an error with the cv2 library.
-# Either try reinstalling cv2 OR try the instructions for TwinMaker, below
-
+# Got this message after creating IoT TwinMaker Workspace via the AWS console
 # It is recommended to create a new IAM policy called CookieFactorySceneViewerPolicy using this template and attach the policy to an IAM role CookieFactoryDashboardRole.
 # Template is saved here as cookie_factory_role.json
 
-# Afterwards, move onto step 4 of the instructions
+# Run the following script to create a role for accessing the workspace in a Grafana dashboard
+# Note the ARN of the role you create. You will use it when configuring a data source in Grafana.
 
-# tried yum install libgl1-mesa-glx
-# got the following error:
-# No package libgl1-mesa-glx available.
+# uncomment the line below the first time to get the Role ARN for Grafana
+# python3 $GETTING_STARTED_DIR/src/modules/grafana/create_grafana_dashboard_role.py --workspace-id $WORKSPACE_ID --region $AWS_DEFAULT_REGION
 
-# I think the issue is that certain packages are not available for Amazon Linux
-# Workaround - try creating a new Cloud 9; can run deploy.sh from where I left off. Don't need to create new IoT Workspace. The Role ARN is still the same
+
+# It seems that the above can't be run in Amazon Linux. May be due to lack of support for OpenCV
+# Also, have added a few extra steps in the pip install, see above
+
+# Deploy an Instance of the Timestream Telemetry module.
+# cd $GETTING_STARTED_DIR/src/modules/timestream_telemetry/cdk/
+# npm install
+# cdk deploy
+
+
+# STUCK ABOVE; MAY BE BECUASE I RAN OUT OF DISK SPACE
+# TRY MOVING TO ANOTHER UBUNTU MACHINE WITH MORE DISK SPACE
+# Instrucions here: https://docs.aws.amazon.com/cloud9/latest/user-guide/move-environment.html
+
+
+
+
+
+
+# cd $GETTING_STARTED_DIR/src/workspaces/cookiefactory/
+
+# import cookie factory data into your workspace
+# python3 -m setup_content \
+#   --telemetry-stack-name $TIMESTREAM_TELEMETRY_STACK_NAME \
+#   --workspace-id $WORKSPACE_ID \
+#   --region-name $AWS_DEFAULT_REGION \
+#   --import-all
